63459510c4ccf58ff240e2d794a5cfe31e19b8b7
BKarayilmaz <63595909+BKarayilmaz@users.noreply.github.com>
2022-03-12 22:15:45 +0300

    Add project files.

---
+++ EfCore/Migrations/20220312190821_InitialCreate.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EfCore.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Categories",
+                columns: table => new
+                {
+                    CategoryID = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    CategoryName = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Categories", x => x.CategoryID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Products",
+                columns: table => new
+                {
+                    ProductID = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    ProductName = table.Column<string>(maxLength: 100, nullable: false),
+                    ProductPrice = table.Column<decimal>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Products", x => x.ProductID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Categories");
+
+            migrationBuilder.DropTable(
+                name: "Products");
+        }
+    }
+}
---
+++ EfCore/Program.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace EfCore
+{
+    public class ShopContext:DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=ShopDb.dp");
+        }
+    }
+    public class Product
+    {
+        //Primary Key(ID, <type_name>Id)
+        [Key]
+        public int ProductID { get; set; }
+        [MaxLength(100)] //Max 100 Character
+        [Required] // Can Not Be Null
+        public string ProductName { get; set; }
+        public decimal ProductPrice { get; set; }//Decimal value type can not be null
+    }
+    public class Category
+    {
+        [Key]
+        public int CategoryID { get; set; }
+        [Required] // Can Not Be Null
+        public string CategoryName { get; set; }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
